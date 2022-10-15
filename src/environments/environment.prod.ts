b9e4bdcf8f6d1624be8bacb40e4d62a0a8ccc741
psuwanarat365 <psuwanarat365@gmail.com>
2022-10-15 10:13:46 +0700

    Connect Firestore with Angular App 1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
+// src/environments/environment.production.ts
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyAMFwG7LopQ5T_aPowxqKvWL0g6-fUVOe4',
+    authDomain: 'ang-blog-f4e3d.firebaseapp.com',
+    projectId: 'ang-blog-f4e3d',
+    storageBucket: 'ang-blog-f4e3d.appspot.com',
+    messagingSenderId: '514315586720',
+    appId: '1:514315586720:web:fffa5548961d96eb040dc5',
+  },
 };
