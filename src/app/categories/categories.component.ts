96a001bb85ddc547ba023fbaa3a515b6555ce6b1
psuwanarat365 <psuwanarat365@gmail.com>
2022-10-15 13:16:41 +0700

    Task and Solution

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -1,15 +1,21 @@
+//app/categories/categories.component.ts
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
-  styleUrls: ['./categories.component.css']
+  styleUrls: ['./categories.component.css'],
 })
 export class CategoriesComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onSubmit(formData) {
+    // console.log(formData.value);
+    let categoryData = {
+      category: formData.value.category,
+    };
+    console.log(categoryData);
   }
-
 }
